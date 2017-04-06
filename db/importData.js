88cdff6a160bfffd8df6ef06eb65c4a8f113330a
xutopia <xuology@gmail.com>
2017-04-06 19:53:37 -0400

    create connection and upload JSON data to mlab

--- db/importData.js
+++ db/importData.js
@@ -1,3 +1,23 @@
 // Import data into mongoDB
 require('dotenv').config();
-var data = require('./readData');
+var mongoose = require('mongoose');
+var data = require('./dataManipulation/readData');
+var Location = require('./models/location');
+
+var mongoURI = process.env.mongoURI;
+console.log('here is the URI', mongoURI);
+mongoose.connect(mongoURI);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  var locations = data.response.locations;
+
+  Location.create(locations)
+  .then(function(locations) {
+    console.log('success!');
+  })
+  .catch(function(err) {
+    console.error(err);
+  })
+})
